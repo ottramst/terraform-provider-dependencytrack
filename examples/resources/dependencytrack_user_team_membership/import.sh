bf04140c8967d253fff395011f20eb8f0e77ff81
Ott Ramst <ott.ramst@gmail.com>
2025-10-29 19:08:27 +0200

    fix: Change team_uuid to team in user_team_membership

---
+++ examples/resources/dependencytrack_user_team_membership/import.sh
@@ -0,0 +1,2 @@
+# User team memberships can be imported using the format: username/team_uuid
+terraform import dependencytrack_user_team_membership.example john.doe/00000000-0000-0000-0000-000000000001
